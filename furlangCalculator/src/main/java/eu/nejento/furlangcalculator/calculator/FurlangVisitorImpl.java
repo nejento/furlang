c8f9b14b42a81f962b28243faec8ff36fafcfc23
Kristián Jacik <nejento@hotmail.cz>
2023-06-22 19:31:02 +0200

    Works.

--- furlangCalculator/src/main/java/eu/nejento/furlangcalculator/calculator/FurlangVisitorImpl.java
+++ furlangCalculator/src/main/java/eu/nejento/furlangcalculator/calculator/FurlangVisitorImpl.java
@@ -1,8 +1,65 @@
 package eu.nejento.furlangcalculator.calculator;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 public class FurlangVisitorImpl extends FurlangBaseVisitor<String> {
+
+    private static final HashMap<String, Integer> furryNumbers = new HashMap<String, Integer>() {{
+        put("Pes", 0);
+        put("Kočka", 1);
+        put("Mýval", 2);
+        put("Vlk", 3);
+        put("Liška", 4);
+        put("Lev", 5);
+        put("Drak", 6);
+        put("Jelen", 7);
+        put("Kůň", 8);
+        put("Protogen", 9);
+    }};
+
+    private static final HashMap<Integer, String> numberFurries = new HashMap<Integer, String>() {{
+        put(0, "Pes");
+        put(1, "Kočka");
+        put(2, "Mýval");
+        put(3, "Vlk");
+        put(4, "Liška");
+        put(5, "Lev");
+        put(6, "Drak");
+        put(7, "Jelen");
+        put(8, "Kůň");
+        put(9, "Protogen");
+    }};
+
+    private String furryToInt(String furry) {
+        String furries = furry.substring(1);
+        StringBuilder sb = new StringBuilder();
+        while (furries.length() > 0) {
+            boolean didFind = false;
+            for (int i = 0; i < furryNumbers.size(); i++) {
+                if (furries.startsWith(numberFurries.get(i))) {
+                    sb.append(i);
+                    furries = furries.substring(numberFurries.get(i).length());
+                    didFind = true;
+                }
+            }
+            if (!didFind) {
+                System.out.println("Invalid furry number (" + furry + ")");
+                return null;
+            }
+        }
+        return sb.toString();
+    }
+
+    private String intToFurry(int number) {
+        String numberString = Integer.toString(number);
+        StringBuilder sb = new StringBuilder();
+        sb.append("f");
+        for (int i = 0; i < numberString.length(); i++) {
+             sb.append(numberFurries.get(Integer.parseInt(numberString.substring(i, i + 1))));
+        }
+        return sb.toString();
+    }
 
     private String handleInt(String left, String operator, String right) {
         int L = 0;
@@ -20,7 +77,7 @@ public class FurlangVisitorImpl extends FurlangBaseVisitor<String> {
                 return Integer.toString(L & R);
             }
             default -> {
-                System.out.print("Calculator does not support " + operator + "\n");
+                System.out.println("Calculator does not support " + operator);
                 return null;
             }
         }
@@ -42,10 +99,88 @@ public class FurlangVisitorImpl extends FurlangBaseVisitor<String> {
                 return "b" + Integer.toBinaryString(LI % RI);
             }
             default -> {
-                System.out.print("Calculator does not support " + operator + "\n");
+                System.out.println("Calculator does not support " + operator);
                 return null;
             }
         }
+    }
+
+    public String handleLeftFurry(String left, String operator, String right) {
+        // fVlk + 2 = fLev
+        // fVlk + fPes = fKůň
+
+        String L = furryToInt(left);
+        if (L == null) return null;
+        int Ln = Integer.parseInt(L);
+
+        int Rn;
+        if (right.toLowerCase(Locale.ROOT).startsWith("f")) {
+            String R = furryToInt(right);
+            if (R == null) return null;
+            Rn = Integer.parseInt(R);
+        } else Rn = Integer.parseInt(right);
+        //System.out.println("handleLeftFurry Right: " + right + " Rn: " + Rn);
+
+        switch (operator) {
+            case "+" -> {
+                //System.out.println("handleLeftFurry: " + intToFurry(Ln + Rn));
+                return intToFurry(Ln + Rn);
+            }
+            case "*" -> {
+                //System.out.println("handleLeftFurry: " + intToFurry(Ln * Rn));
+                return intToFurry(Ln * Rn);
+            }
+            case "%" -> {
+                //System.out.println("handleLeftFurry: " + intToFurry(Ln % Rn));
+                return intToFurry(Ln % Rn);
+            }
+            default -> {
+                System.out.println("Calculator does not support " + operator);
+                return null;
+            }
+        }
+    }
+
+    private String handleRightFurry(String left, String operator, String right) {
+        int Ln = 0;
+        if (left.toLowerCase(Locale.ROOT).startsWith("f")) {
+            String L = furryToInt(left);
+            if (L != null) Integer.parseInt(L);
+        } else Ln = Integer.parseInt(left);
+
+        String R = furryToInt(right);
+        int Rn = 0;
+        if (R == null) return null;
+        Rn = Integer.parseInt(R);
+
+        switch (operator) {
+            case "+" -> {
+                //System.out.println("handleRightFurry: " + intToFurry(Ln + Rn));
+                return Integer.toString(Ln + Rn);
+            }
+            case "*" -> {
+                //System.out.println("handleRightFurry: " + intToFurry(Ln * Rn));
+                return Integer.toString(Ln * Rn);
+            }
+            case "%" -> {
+                //System.out.println("handleRightFurry: " + intToFurry(Ln % Rn));
+                return Integer.toString(Ln % Rn);
+            }
+            default -> {
+                System.out.println("Calculator does not support " + operator);
+                return null;
+            }
+        }
+    }
+
+    private static boolean isNumeric(String strNum) {
+        if (strNum == null) return false;
+        try {
+            int d = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -69,6 +204,8 @@ public class FurlangVisitorImpl extends FurlangBaseVisitor<String> {
         String operator = ctx.operator.getText();
         System.out.println("Operator: " + operator);
 
+        System.out.println("Opexpr: " + ctx.getText());
+
         boolean b = right.toLowerCase(Locale.ROOT).startsWith("b0") || right.toLowerCase(Locale.ROOT).startsWith("b1");
         if (b) {
             if (left == null || left.toLowerCase(Locale.ROOT).startsWith("b0") || left.toLowerCase(Locale.ROOT).startsWith("b1")) {
@@ -81,7 +218,25 @@ public class FurlangVisitorImpl extends FurlangBaseVisitor<String> {
             return null;
         }
 
-        System.out.println("Opexpr: " + ctx.getText());
+        // fVlk + 2 = fLev    //left furry operation
+        // fVlk + fPes = fKůň //left furry operation
+        // 2 + fVlk = 5       //right furry operation
+        // null + fVlk = 3    //right furry operation
+
+        if (left.toLowerCase(Locale.ROOT).startsWith("f")) {
+            if (right.toLowerCase(Locale.ROOT).startsWith("f") || isNumeric(right)) {
+                return handleLeftFurry(left, operator, right);
+            } else if (right.toLowerCase(Locale.ROOT).startsWith("\"") && operator.equals("*")) {
+                return right.substring(1, right.length() - 1).repeat(Integer.parseInt(furryToInt(left)));
+            }
+        } else if (right.toLowerCase(Locale.ROOT).startsWith("f")) {
+            if (left.toLowerCase(Locale.ROOT).startsWith("f") || isNumeric(left)) {
+                return handleRightFurry(left, operator, right);
+            } else if (left.toLowerCase(Locale.ROOT).startsWith("\"") && operator.equals("*")) {
+                return left.substring(1, left.length() - 1).repeat(Integer.parseInt(furryToInt(right)));
+            }
+        }
+
 
         // Konkatenace stringů "String" + "String"
         if (left.toLowerCase(Locale.ROOT).startsWith("\"") && right.toLowerCase(Locale.ROOT).startsWith("\"") && operator.equals("+")) {
@@ -89,30 +244,21 @@ public class FurlangVisitorImpl extends FurlangBaseVisitor<String> {
         }
         // Else multiplikace stringů s číslem "String" * 3 = "StringStringString"
         else if (left.toLowerCase(Locale.ROOT).startsWith("\"") && operator.equals("*")) {
-            int R = Integer.parseInt(right);
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < R; i++) {
-                result.append(left, 1, left.length() - 1);
-            }
-            return result.toString();
+            return left.substring(1, left.length() - 1).repeat(Integer.parseInt(right));
         }
         // Else multiplikace stringů s číslem, jen přehozené strany
         else if (right.toLowerCase(Locale.ROOT).startsWith("\"") && operator.equals("*")) {
-            int L = Integer.parseInt(left);
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < L; i++) {
-                result.append(right, 1, right.length() - 1);
-            }
-            return result.toString();
-        } else {
-            return handleInt(left, operator, right);
+            return right.substring(1, right.length() - 1).repeat(Integer.parseInt(left));
         }
+
+        return handleInt(left, operator, right);
 
     }
 
     @Override
     public String visitParexpr(FurlangParser.ParexprContext ctx) {
         System.out.println("Parexpr: " + ctx.getText());
+        System.out.println(ctx.expr().getText());
         return this.visit(ctx.expr());
     }
 
@@ -127,6 +273,9 @@ public class FurlangVisitorImpl extends FurlangBaseVisitor<String> {
         // If leafexp is bindec(b101) convert the binary to decimal number
         if (ctx.getText().toLowerCase(Locale.ROOT).startsWith("bindec(b") && ctx.getText().endsWith(")"))
             return Integer.toString(Integer.parseInt(ctx.getText().substring(8, ctx.getText().length() - 1), 2));
+
+        if (ctx.getText().toLowerCase(Locale.ROOT).startsWith("furry(") && ctx.getText().endsWith(")"))
+            return intToFurry(Integer.parseInt(ctx.getText().substring(6, ctx.getText().length() - 1)));
 
         return ctx.getText();
     }
